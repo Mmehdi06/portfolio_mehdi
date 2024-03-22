db60d20ff7c863fbdb4b73938aacc511d098ba10
Mehdi Merkachi <merkachimehdi@gmail.com>
2024-03-22 16:12:28 +0100

    Add new SVG icons for HTML, RabbitMQ, JavaScript, Tailwind CSS, CSS, Git, AdonisJS, C#, and Spring Boot

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,11 @@ interface IConfig {
     };
   };
   socials: {
-    [name: string]: string;
+    [name: string]: {
+      name: string;
+      url: string;
+      icon: string;
+    };
   };
   projects: {
     [name: string]: {
@@ -45,133 +49,139 @@ export const Config: IConfig = {
     Languages: {
       java: {
         name: "Java",
-        icon: "/src/assets/icons/javaIcon.svg",
+        icon: "/src/assets/icons/java.svg",
         url: "https://www.java.com/",
       },
       cSharp: {
         name: "C#",
-        icon: "/src/assets/icons/csharpIcon.svg",
+        icon: "/src/assets/icons/csharp.svg",
         url: "https://docs.microsoft.com/en-us/dotnet/csharp/",
       },
       javascript: {
         name: "Javascript",
-        icon: "/src/assets/icons/javascriptIcon.svg",
+        icon: "/src/assets/icons/javascript.svg",
         url: "https://w3schools.com/js/",
       },
       typescript: {
         name: "TypeScript",
-        icon: "/src/assets/icons/typescriptIcon.svg",
+        icon: "/src/assets/icons/typescript.svg",
         url: "https://www.typescriptlang.org/",
       },
       python: {
         name: "Python",
-        icon: "/src/assets/icons/pythonIcon.svg",
+        icon: "/src/assets/icons/python.svg",
         url: "https://www.python.org/",
       },
       php: {
         name: "PHP",
-        icon: "/src/assets/icons/phpIcon.svg",
+        icon: "/src/assets/icons/php.svg",
         url: "https://www.php.net/",
       },
       html: {
         name: "HTML",
-        icon: "/src/assets/icons/htmlIcon.svg",
+        icon: "/src/assets/icons/html.svg",
         url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
       },
       css: {
         name: "CSS",
-        icon: "/src/assets/icons/cssIcon.svg",
+        icon: "/src/assets/icons/css.svg",
         url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
       },
     },
     Frontend: {
       react: {
         name: "React",
-        icon: "/src/assets/icons/reactIcon.svg",
+        icon: "/src/assets/icons/react.svg",
         url: "https://reactjs.org/",
       },
       solidjs: {
         name: "SolidJS",
-        icon: "/src/assets/icons/solidjsIcon.svg",
+        icon: "/src/assets/icons/solidjs.svg",
         url: "https://solidjs.com/",
       },
       astro: {
         name: "Astro",
-        icon: "/src/assets/icons/astroIcon.svg",
+        icon: "/src/assets/icons/astro.svg",
         url: "https://astro.build/",
       },
-      // Add more frontend frameworks here
     },
     Backend: {
       nodejs: {
         name: "Node.js",
-        icon: "/src/assets/icons/nodejsIcon.svg",
+        icon: "/src/assets/icons/nodejs.svg",
         url: "https://nodejs.org/",
       },
       laravel: {
         name: "Laravel",
-        icon: "/src/assets/icons/laravelIcon.svg",
+        icon: "/src/assets/icons/laravel.svg",
         url: "https://laravel.com/",
       },
       adonisjs: {
         name: "AdonisJS",
-        icon: "/src/assets/icons/adonisjsIcon.svg",
+        icon: "/src/assets/icons/adonisjs.svg",
         url: "https://adonisjs.com/",
       },
       dotnet: {
         name: ".NET",
-        icon: "/src/assets/icons/dotnetIcon.svg",
+        icon: "/src/assets/icons/dotnet.svg",
         url: "https://dotnet.microsoft.com/",
       },
-      // Add more backend frameworks here
+      springBoot: {
+        name: "Spring Boot",
+        icon: "/src/assets/icons/springboot.svg",
+        url: "https://spring.io/projects/spring-boot",
+      },
     },
     Extras: {
       tailwindcss: {
         name: "TailwindCSS",
-        icon: "/src/assets/icons/tailwindcssIcon.svg",
+        icon: "/src/assets/icons/tailwindcss.svg",
         url: "https://tailwindcss.com/",
       },
       git: {
         name: "Git",
-        icon: "/src/assets/icons/gitIcon.svg",
+        icon: "/src/assets/icons/git.svg",
         url: "https://git-scm.com/",
       },
       docker: {
         name: "Docker",
-        icon: "/src/assets/icons/dockerIcon.svg",
+        icon: "/src/assets/icons/docker.svg",
         url: "https://www.docker.com/",
       },
       linux: {
         name: "Linux",
-        icon: "/src/assets/icons/linuxIcon.svg",
+        icon: "/src/assets/icons/linux.svg",
         url: "https://www.linux.org/",
       },
       rabbitmq: {
         name: "RabbitMQ",
-        icon: "/src/assets/icons/rabbitmqIcon.svg",
+        icon: "/src/assets/icons/rabbitmq.svg",
         url: "https://www.rabbitmq.com/",
       },
       redis: {
         name: "Redis",
-        icon: "/src/assets/icons/redisIcon.svg",
+        icon: "/src/assets/icons/redis.svg",
         url: "https://redis.io/",
       },
     },
   },
 
   socials: {
-    // twitter: "https://twitter.com/mehdimerkachi",
-    github: "https://github.com/Mmehdi06",
-    linkedin: "https://linkedin.com/in/mehdimerkachi",
+    githubObject: {
+      name: "GitHub",
+      url: "https://github.com/Mmehdi06",
+      icon: "/src/assets/icons/github.svg",
+    },
+    linkedinObject: {
+      name: "LinkedIn",
+      url: "https://linkedin.com/in/mehdimerkachi",
+      icon: "/src/assets/icons/linkedin.svg",
+    },
   },
   projects: {
     "This Website": {
-      url: "https://github.com/996icu/996.ICU",
+      url: "https://github.com/Mmehdi06/portfolio_mehdi",
       tags: ["Astro", "TypeScript", "TailwindCSS"],
-    },
-    Linux: {
-      url: "https://github.com/torvalds/linux",
-      tags: ["c", "kernel", "unix", "os"],
     },
   },
   og: {
