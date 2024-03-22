966b36ededb9a5252affadf141540d55c853db08
Mehdi Merkachi <merkachimehdi@gmail.com>
2024-03-22 17:16:39 +0100

    Delete unused files and update fonts and icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -5,6 +7,9 @@ export default {
     extend: {
       colors: {
         "paper-color": "#f6eee3",
+      },
+      fontFamily: {
+        sans: ["IBMPlexMono", ...defaultTheme.fontFamily.sans],
       },
     },
   },
